16a96398ac8eb3cc5fdb6624aaf24a04bb35cb9e
Emme Porter DeWitt <empdewitt@gmail.com>
2018-01-15 03:13:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-var addElementToBeginningofArray = new Array();
-var addElementToBeginningofArray = ["array", "element"];
+var addElementToBeginningofArray = [array, element];
 addElementToBeginningofArray.unshift("element");
 var destructivelyAddElementToBeginningOfArray = ["array", "element"];
 destructivelyAddElementToBeginningOfArray.unshift("element");
